7b5b6d2e29376915cac73f28caca20a7f329d0fc
Fred <frederico.paula@abril.com.br>
2011-12-16 15:07:34 -0200

    Caching gem info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,13 +19,19 @@ class HomeController < ApplicationController
   
   private
   
-  def gem_info    
+  def gem_info   
+    cache_key = 'gem-info'
+    result = Rails.cache.read(cache_key)
+    return result if result
+    puts 'nao pegou cache'
     uri = URI.parse('https://rubygems.org/api/v1/gems/text_parser.json')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
-    JSON.parse(response.body)
+    result = JSON.parse(response.body)
+    Rails.cache.write(cache_key, result, :expires_in=>30.seconds)
+    return result
   end
 end
