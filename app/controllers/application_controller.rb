c408c21f94202aeea70ed6b8dd203fa0b0c8beeb
Fred <fpaula@gmail.com>
2013-01-31 13:29:36 -0200

    Checking if rubygems api is ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ require 'json'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   private
-  
-  def gem_info   
+
+  def gem_info
     cache_key = 'gem-info'
     result = Rails.cache.read(cache_key)
     return result if result
@@ -17,8 +17,14 @@ class ApplicationController < ActionController::Base
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
-    result = JSON.parse(response.body)
-    Rails.cache.write(cache_key, result, :expires_in => 50.seconds)
+
+    result = nil
+    p response.code
+    if response.code == '200'
+      result = JSON.parse(response.body)
+      Rails.cache.write(cache_key, result, :expires_in => 50.seconds)
+    end
+
     return result
   end
 end
