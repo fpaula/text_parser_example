97a0fd6062a5a08e99fa77728cf1d313c06718a5
Fred <fpaula@gmail.com>
2013-09-13 17:56:42 -0300

    test

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,12 +1,5 @@
 class ExampleController < ApplicationController
   def index
     @gem_info = gem_info
-    @user_ip = [
-      request.env["HTTP_X_FORWARDED_FOR"],
-      request.env['HTTP_X_REAL_IP'],
-      request.env['REMOTE_ADDR'],
-      request.remote_ip,
-      request.remote_addr
-    ]
   end
 end
