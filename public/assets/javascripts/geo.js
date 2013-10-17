8452111f352eda3bf6a1b7b85b08aa17a138a235
Fred <fpaula@gmail.com>
2013-10-17 18:43:29 -0300

    Testing geo location

---
+++ public/assets/javascripts/geo.js
@@ -0,0 +1,40 @@
+var Geo = {
+  _userGeo: null,
+  _watchId: null,
+  get: function(successCallback) {
+    if (this._userGeo) {
+      return successCallback(this._userGeo);
+    }
+    navigator.geolocation.getCurrentPosition(
+      function(position){
+        Geo._userGeo = position;
+        successCallback(position);
+      },
+      this.error, this.options
+    );
+  },
+  watch: function(successCallback) {
+    this._watchId = navigator.geolocation.watchPosition(
+      function(position) {
+        successCallback(position);
+    },
+    this.error, this.options);
+  },
+  clearWatch: function() {
+    navigator.geolocation.clearWatch(this._watchId);
+    this._watchId = null;
+    return true;
+  },
+  _createPrintElement: function (key, value) {
+    return $("<li>" + key + ": " + value + "</li>");
+  },
+  _error: function(error) {
+    console.warn('ERROR(' + error.code + '): ' + error.message);
+  },
+  _options: function() {
+    //TODO: options as params
+    return { enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0 }
+  }
+};
